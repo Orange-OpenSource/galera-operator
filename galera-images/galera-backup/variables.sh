92a4d9fb59e63c4df7034266c2a1e622813214ab
sebs42 <sebastien.spagnolo@orange.com>
2020-03-27 17:21:47 +0100

    URL Images correction

---
+++ galera-images/galera-backup/variables.sh
@@ -0,0 +1,16 @@
+
+GB_S3Login="sebs42"
+GB_S3Password="miniotest"
+GB_S3Endpoint="http://minio.apps-rocks.fr"
+GB_S3Bucket="gal"
+GB_S3BackupName="go-test"
+GB_Backup="/Users/seb/dev/galera/galera-images/galera-backup"
+GB_Restore="./test/restore"
+
+export GB_S3Login
+export GB_S3Password
+export GB_S3Endpoint
+export GB_S3Bucket
+export GB_S3BackupName
+export GB_Backup
+export GB_Restore
